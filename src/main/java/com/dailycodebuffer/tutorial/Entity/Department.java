a9080da210405b4e45e78735cb5248570dd1c125
telman03 <telmangadimov1@gmail.com>
2022-11-28 17:50:57 +0400

    Message to fill the requested blank

--- src/main/java/com/dailycodebuffer/tutorial/Entity/Department.java
+++ src/main/java/com/dailycodebuffer/tutorial/Entity/Department.java
@@ -5,12 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long departmentId;
+
+    @NotBlank(message = "Please Add Department Name")
     private String departmentName;
     private String departmentAddress;
     private String departmentCode;
