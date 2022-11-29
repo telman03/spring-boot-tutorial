fbb0542c8392b37b16c746dffe7c9815a4e6b157
telman03 <telmangadimov1@gmail.com>
2022-11-29 17:42:08 +0400

    H2 -> PostgresSQL

--- src/main/java/com/dailycodebuffer/tutorial/Error/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/dailycodebuffer/tutorial/Error/RestResponseEntityExceptionHandler.java
@@ -3,15 +3,15 @@ package com.dailycodebuffer.tutorial.Error;
 import com.dailycodebuffer.tutorial.Entity.ErrorMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@Controller
+@ControllerAdvice
 @ResponseStatus
-public class RestResponseEntityHandler extends ResponseEntityExceptionHandler {
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
         @ExceptionHandler(DepartmentNotFoundException.class)
         public ResponseEntity<ErrorMessage> departmentNotFoundException(DepartmentNotFoundException exception,
                                                                         WebRequest request){
